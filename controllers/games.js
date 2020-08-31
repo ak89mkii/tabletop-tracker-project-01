4282a4482a46b8e62d04b148e3f22f1d06e539b6
A.I. Kasem <ak89mkii@gmail.com>
2020-08-30 23:56:29 -0400

    Familiar API test integration successful

--- controllers/games.js
+++ controllers/games.js
@@ -1,14 +1,29 @@
 const Game = require('../models/game')
+const axios = require('axios')
 
 module.exports = {
 //     new: newGames,
 //     create, 
-    index,
+  index,
 //     show,
 //     addToLibrary,
 //     addToWishlist,
 //     addToBring,
 //     addToVotePage
+  pokeView,
+  pokeQuery
+}
+
+function pokeView(req, res) {
+  res.render('games/new', { pokemon: null })
+}
+
+function pokeQuery(req, res) {
+  axios.get(`https://pokeapi.co/api/v2/pokemon/${req.body.query.toLowerCase()}`)
+  .then(response => {
+      console.log(response.data.species.name)
+      res.render('games/new', { pokemon: response.data })
+  })
 }
 
 function index(req, res) {
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Ctrl = require('../controllers/pokemons')
+const Ctrl = require('../controllers/games')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
