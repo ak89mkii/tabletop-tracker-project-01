192e3e4378797246e6f3eba80e287e384805698c
A.I. Kasem <ak89mkii@gmail.com>
2020-09-02 01:37:54 -0400

    Flexbox

--- models/wishlist.js
+++ models/wishlist.js
@@ -5,7 +5,7 @@ const wishlistSchema = new Schema({
     imageUrl: {type: String},
     title: {type: String},
     priority: {type: String, enum: ['Must Purchase', 'Vote on It', 'Not Priority', 'Wait for Sale']},
-    cost: {type: Number}
+    price: {type: Number}
 }, {
   timestamps: true
 })
