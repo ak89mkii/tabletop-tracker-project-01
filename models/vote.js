bc27b39651589bf238c0517f45fefbeaa74c2d90
A.I. Kasem <ak89mkii@gmail.com>
2020-09-02 23:11:20 -0400

    Vote

--- models/vote.js
+++ models/vote.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const voteSchema = new Schema({
+  imageUrl: {type: String},
   title: {type: String},
+  datePosted: {type: Date},
   vote: {type: Number},
   comments: {type: String}
 }, {
